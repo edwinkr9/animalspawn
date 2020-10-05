5f1ed8b8c6c4a2e6ebf5c5d79b2e4a7d1a7a23f3
edwinkr9 <71083780+edwinkr9@users.noreply.github.com>
2020-10-04 19:53:58 -0500

    Add files via upload

---
+++ AnimalSpawn/AnimalSpawn/AnimalSpawn.Api/Controllers/AnimalController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using AnimalSpawn.Domain.Interface;
+using AnimalSpawn.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalSpawn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAnimalRepository _repository;
+        public AnimalController(IAnimalRepository repository)
+        {
+            this._repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var animals = await _repository.GetAnimals();
+            return Ok(animals);
+        }
+
+
+    }
+}
